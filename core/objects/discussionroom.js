ea5c91bfbc24ee520bcee034b22a7ba7992cf7ce
dev0ps221 <seybaxscorpion@gmail.com>
2022-01-20 10:21:25 +0000

    DiscussionRoom :: deebee :: getroomdiscussions,getroomdiscussion << [DONE]!!

--- core/objects/discussionroom.js
+++ core/objects/discussionroom.js
@@ -3,10 +3,20 @@ class DiscussionRoom{
     registerDeeBeeActions(){
         
         this.db._____registerAction(
-            getRoomDiscussion,
+            getRoomDiscussions,
             (roomId,cb)=>{
                 let req = this.db.selecFrom(
                     'roomdiscussions',['*'],[['roomid'],[roomId]]
+                )
+                this.db.db.query(req,cb)
+            }
+        )
+        
+        this.db._____registerAction(
+            getRoomDiscussion,
+            (id,cb)=>{
+                let req = this.db.selecFrom(
+                    'roomdiscussions',['*'],[['id'],[id]]
                 )
                 this.db.db.query(req,cb)
             }
