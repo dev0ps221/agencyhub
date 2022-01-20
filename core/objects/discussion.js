17915fd3725876635156565e852f545d44cc646c
dev0ps221 <seybaxscorpion@gmail.com>
2022-01-20 13:50:36 +0000

    Discussion :: addMessage || Detachement :: deleteMessage << automated add|remove messages...

--- core/objects/discussion.js
+++ core/objects/discussion.js
@@ -2,16 +2,76 @@
 class Discussion{
 
 
-    addMessage(data,cb){
+    registerDeeBeeActions(){
 
+        this.db._____registerAction(
+            'getDiscussionMessages',(discussionid,cb)=>{
+                let req = this.__selectFrom(
+                    'discussionmessages',['*'],[['discussionid'],[discussionid]]
+                )
+                this.db.db.query(req,cb)
+            }
+        )
+
+        this.db._____registerAction(
+            'getDiscussionMessage',(id,cb)=>{
+                let req = this.__selectFrom(
+                    'discussionmessages',['*'],[['id'],[id]]
+                )
+                this.db.db.query(req,cb)
+            }
+        )
+
+        this.db._____registerAction(
+            'addDiscussionMessage',(data,cb)=>{
+                const {content,auteurid,discussionid} = data
+                let req = this.__insertINTO(
+                    'discussionmessages',['discussionid','content','auteurid'],[discussionid,`'${content}'`,auteurid]
+                )
+                this.db.db.query(req,cb)
+            }
+        )
+
+        this.db._____registerAction(
+            'editDiscussionMessage',(data,cb)=>{
+                const {content,id} = data
+                let req = this.__updtWhere(
+                    'discussionmessages',['content'],[`'${content}'`],[['id'],[id]]
+                )
+                this.db.db.query(req,cb)
+            }
+        )
+
+        this.db._____registerAction(
+            'removeDiscussionMessage',(id,cb)=>{
+                let req = this.__delFrom(
+                    'discussionmessages',['id'],[id]
+                )
+                this.db.db.query(req,cb)
+            }
+        )
+
+        
+        
+    }
+
+    addMessage(data,cb){
+        data.discussionid = this.discussionid
+        this.db.addDiscussionMessage(
+            data,cb
+        )
     }
 
     deleteMessage(id,cb){
-
+        this.db.removeDiscussionMessage(
+            id,cb
+        )
     }
 
     editMessage(data,cb){
-
+        this.db.editDiscussionMessage(
+            data,cb
+        )
     }
 
     addMedia(data,cb){
@@ -22,16 +82,30 @@ class Discussion{
 
     }
 
-    setMessages(){
+    assignMessages(messages){
+        messages.forEach(
+            message=>{
+                this.messages.push(message)
+            }
+        )
+    }
 
+    setMessages(){
+        this.db.getDiscussionMessages(this.discussionid,(e,messages)=>{
+            if(e)console.log(e)
+            else{
+                this.assignMessages(messages)
+            }
+        })
     }
 
     getMessages(){
-
+        return this.messages
     }
 
     assignData(data){
         this.id = data.id
+        this.discussionid = this.id
         this.nom = data.name
         this.name = data.name
         this.roomid = data.roomid
